5d37e9635f1e7c50ac7537b36fc63ff473b70dfd
DispatchWorkItem <git>
2024-02-05 23:34:58 -0800

    release 0.9.2

--- FinixPaymentSheet.xcframework/ios-arm64_armv7/FinixPaymentSheet.framework/Headers/FinixPaymentSheet-Swift.h
+++ FinixPaymentSheet.xcframework/ios-arm64_armv7/FinixPaymentSheet.framework/Headers/FinixPaymentSheet-Swift.h
@@ -241,6 +241,7 @@ SWIFT_CLASS("_TtC17FinixPaymentSheet22PaymentInputController")
 
 
 
+
 @class UITextField;
 
 @interface PaymentInputController (SWIFT_EXTENSION(FinixPaymentSheet)) <UITextFieldDelegate>
@@ -505,6 +506,7 @@ SWIFT_CLASS("_TtC17FinixPaymentSheet22PaymentInputController")
 - (nonnull instancetype)initWithStyle:(UITableViewStyle)style SWIFT_UNAVAILABLE;
 - (nonnull instancetype)initWithNibName:(NSString * _Nullable)nibNameOrNil bundle:(NSBundle * _Nullable)nibBundleOrNil SWIFT_UNAVAILABLE;
 @end
+
 
 
 
