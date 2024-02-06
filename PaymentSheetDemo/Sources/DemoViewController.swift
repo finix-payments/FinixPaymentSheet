1ac94e375a741073782edd66745e3da365912adf
DispatchWorkItem <git>
2024-02-05 23:34:58 -0800

    v0.9.8 release

---
+++ PaymentSheetDemo/Sources/DemoViewController.swift
@@ -0,0 +1,289 @@
+//
+//  ViewController.swift
+//  PaymentSheet
+//
+//  Created by Jack Tihon on 6/24/22.
+//
+
+import FinixPaymentSheet
+import UIKit
+
+// Provide your own Application Id
+let APPLICATION_ID = "APgPDQrLD52TYvqazjHJJchM"
+
+let BRANDING_LOGO: UIImage? = #imageLiteral(resourceName: "FinixLogo")
+let BRANDING_NAME = "Daphne's Corner"
+
+/**
+ This controller demonstrates usage of the PaymentSheet.
+  It is expected to be pushed onto a navigation controller for the nav controller presentation styles
+  */
+class DemoViewController: UITableViewController {
+    var paymentSDK: PaymentAction!
+
+    // Provide your own branding
+    // NOTE: provide both light and dark appearances!
+    private let branding: PaymentInputController.Branding = .init(image: BRANDING_LOGO, title: BRANDING_NAME)
+
+    override init(nibName nibNameOrNil: String? = nil, bundle nibBundleOrNil: Bundle? = nil) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    override func viewDidLoad() {
+        let credentials = FinixCredentials(applicationId: APPLICATION_ID, environment: .Sandbox)
+        paymentSDK = .init(credentials: credentials)
+        navigationItem.title = "Card Payment Sheet Demo"
+
+        super.viewDidLoad()
+        tableView.register(DemoCell.self, forCellReuseIdentifier: DemoCell.Identifier)
+        tableView.register(DemoSwitchCell.self, forCellReuseIdentifier: DemoSwitchCell.Identifier)
+    }
+
+    override func numberOfSections(in _: UITableView) -> Int {
+        DemoCellSection.allCases.count
+    }
+
+    override func tableView(_: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch DemoCellSection.allCases[section] {
+        case .modal, .push:
+            return RowStyle.allCases.count
+        case .configuration:
+            return DemoSwitch.allCases.count
+        }
+    }
+
+    override func tableView(_: UITableView, titleForHeaderInSection section: Int) -> String? {
+        DemoCellSection.allCases[section].title
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch DemoCellSection.allCases[indexPath.section] {
+        case .configuration:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: DemoSwitchCell.Identifier) as? DemoSwitchCell else {
+                fatalError("Expected DemoCell")
+            }
+            let demoSwitch = DemoSwitch.allCases[indexPath.row]
+            switch demoSwitch {
+            case .showCountry:
+                cell.switchControl.isOn = showCountry
+                cell.switchControl.addTarget(self, action: #selector(showCountryValueChanged(_:)), for: .valueChanged)
+            case .showCancel:
+                cell.switchControl.isOn = showCancel
+                cell.switchControl.addTarget(self, action: #selector(showCancelButtonValueChanged(_:)), for: .valueChanged)
+            }
+            cell.textLabel?.text = demoSwitch.title
+            return cell
+        case .modal, .push:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: DemoCell.Identifier) as? DemoCell else {
+                fatalError("Expected DemoCell")
+            }
+            cell.textLabel?.text = RowStyle.allCases[indexPath.row].title
+            return cell
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        // trigger action
+        let style: PaymentInputController.Style
+        let row = indexPath.row
+        switch RowStyle.allCases[row] {
+        case .complete:
+            style = .complete
+        case .basic:
+            style = .basic
+        case .partial:
+            style = .partial
+        case .minimal:
+            style = .minimal
+        }
+
+        // setup here for `showsCancel`
+        setupPaymentSDK()
+
+        switch DemoCellSection.allCases[indexPath.section] {
+        case .modal:
+            modalPresentSheet(style: style)
+        case .push:
+            navigationPushSheet(style: style)
+        case .configuration:
+            return
+        }
+    }
+
+    // configuration
+    private var showCountry: Bool = false
+    private var showCancel: Bool = false
+}
+
+enum DemoCellSection: Int, CaseIterable {
+    case modal
+    case push
+    case configuration
+
+    var title: String {
+        switch self {
+        case .modal:
+            return "Modal Presentation"
+        case .push:
+            return "Push Presentation"
+        case .configuration:
+            return "Configuration"
+        }
+    }
+}
+
+enum RowStyle: Int, CaseIterable {
+    case complete
+    case basic
+    case partial
+    case minimal
+
+    var title: String {
+        switch self {
+        case .complete:
+            return "complete"
+        case .basic:
+            return "basic"
+        case .partial:
+            return "partial"
+        case .minimal:
+            return "minimal"
+        }
+    }
+}
+
+enum DemoSwitch: Int, CaseIterable {
+    case showCountry
+    case showCancel
+
+    var title: String {
+        switch self {
+        case .showCancel:
+            return "Show Cancel Button"
+        case .showCountry:
+            return "Show Country"
+        }
+    }
+}
+
+// MARK: PaymentSheet presentation
+
+extension DemoViewController {
+    private func setupPaymentSDK() {
+        // Set up configuration
+        paymentSDK.configuration = .init(title: "Card Entry", branding: branding, buttonTitle: "Tokenize", showsCancelButton: showCancel)
+        // Designate a delegate
+        paymentSDK.delegate = self
+    }
+
+    private func modalPresentSheet(style: PaymentInputController.Style) {
+        let paymentController = paymentSDK.paymentSheet(style: style, showCountry: showCountry)
+        paymentSDK.present(from: self, paymentSheet: paymentController)
+    }
+
+    private func navigationPushSheet(style: PaymentInputController.Style) {
+        let paymentSheet = paymentSDK.paymentSheet(style: style, showCancelItem: false, showCountry: showCountry)
+        navigationController?.pushViewController(paymentSheet, animated: true)
+    }
+}
+
+// MARK: PaymentActionDelegate
+
+extension DemoViewController: PaymentActionDelegate {
+    // display result
+    func didSucceed(paymentController: PaymentInputController, instrument: TokenResponse) {
+        debugPrint("got TokenizedCard: \(paymentController),\(instrument)")
+
+        let resultController = ResultViewController()
+        resultController.result = .success(instrument)
+        debugPrint("""
+        Got a token response with:
+                id: \(instrument.id)
+                fingerprint: \(instrument.fingerprint)
+                created: \(instrument.created)
+                updated: \(instrument.updated)
+                instrument: \(instrument.instrument)
+                expires: \(instrument.expires)
+                isoCurrency: \(instrument.isoCurrency)
+        """)
+
+        paymentController.navigationController?.pushViewController(resultController, animated: true)
+    }
+
+    func didCancel(paymentController _: PaymentInputController) {
+        debugPrint("cancel tapped")
+        dismiss(animated: true)
+    }
+
+    func didFail(paymentController: PaymentInputController, error: Error) {
+        debugPrint("failed to process with error: \(error)")
+
+        let resultController = ResultViewController()
+        if let error = error as? FinixError {
+            debugPrint("FinixError with \(error.message), code: \(error.code)")
+        }
+        resultController.result = .error(error)
+        paymentController.navigationController?.pushViewController(resultController, animated: true)
+    }
+}
+
+extension DemoViewController {
+    @IBAction
+    func showCountryValueChanged(_ switchView: UISwitch) {
+        showCountry = switchView.isOn
+    }
+
+    @IBAction
+    func showCancelButtonValueChanged(_ switchView: UISwitch) {
+        showCancel = switchView.isOn
+    }
+}
+
+// MARK: TableCellIdentifier
+
+protocol TableCellIdentifiable: AnyObject {
+    static var Identifier: String { get }
+}
+
+extension UITableViewCell: TableCellIdentifiable {
+    static var Identifier: String { String(describing: Self.self) }
+}
+
+// MARK: DemoCell
+
+class DemoCell: UITableViewCell {
+    override init(style _: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .default, reuseIdentifier: reuseIdentifier)
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: DemoSwitchCell
+
+class DemoSwitchCell: UITableViewCell {
+    let switchControl: UISwitch = .init()
+
+    override init(style _: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .default, reuseIdentifier: reuseIdentifier)
+        accessoryView = switchControl
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        switchControl.removeTarget(nil, action: nil, for: .valueChanged)
+    }
+}
